79cc57b464d8b8a621662fdbdca9227fffe67274
ingoxx <pythongolangvue@gmail.com>
2025-03-20 16:11:40 +0800

    first commit

---
+++ api/v1/nginxingress_webhook.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// log is for logging in this package.
+var nginxingresslog = logf.Log.WithName("nginxingress-resource")
+
+func (r *NginxIngress) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+//+kubebuilder:webhook:path=/mutate-ingress-ingress-k8s-io-v1-nginxingress,mutating=true,failurePolicy=fail,sideEffects=None,groups=ingress.ingress-k8s.io,resources=nginxingresses,verbs=create;update,versions=v1,name=mnginxingress.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Defaulter = &NginxIngress{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (r *NginxIngress) Default() {
+	nginxingresslog.Info("default", "name", r.Name)
+
+	// TODO(user): fill in your defaulting logic.
+}
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+//+kubebuilder:webhook:path=/validate-ingress-ingress-k8s-io-v1-nginxingress,mutating=false,failurePolicy=fail,sideEffects=None,groups=ingress.ingress-k8s.io,resources=nginxingresses,verbs=create;update,versions=v1,name=vnginxingress.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Validator = &NginxIngress{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *NginxIngress) ValidateCreate() error {
+	nginxingresslog.Info("validate create", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object creation.
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *NginxIngress) ValidateUpdate(old runtime.Object) error {
+	nginxingresslog.Info("validate update", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object update.
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *NginxIngress) ValidateDelete() error {
+	nginxingresslog.Info("validate delete", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
